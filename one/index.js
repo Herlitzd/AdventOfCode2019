8c556315e3cf44f325c56eddc94e5f191e611e95
Devon Herlitz <devonherlitz@gmail.com>
2019-12-01 16:34:44 -0500

    day 1: part 2

--- one/index.js
+++ one/index.js
@@ -4,9 +4,18 @@ const calculateMass = (mass) => {
   return Math.abs(Math.floor(mass / 3) - 2);
 }
 
+const calculateMass2 = (mass) => {
+  let m = Math.floor(mass / 3) - 2;
+  if(m <= 0){
+    return 0;
+  }else{
+    return m + calculateMass2(m);
+  }
+}
+
 fs.readFile("input.json", (err, rawData) => {
   let data = JSON.parse(rawData.toString()).data;
-  const out = data.reduce((prev, curr) => prev + calculateMass(curr), 0)
+  const out = data.reduce((prev, curr) => prev + calculateMass2(curr), 0)
   console.log(out);
 });
 
